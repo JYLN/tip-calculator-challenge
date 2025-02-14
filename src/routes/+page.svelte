bd1a43010c91ab3c31b2c7e00f47f899b42d2176
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2025-02-13 20:04:07 -0700

    chore: :tada: init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+</script>
+
+<svelte:head>
+	<title>Frontend Mentor | Tip Calculator</title>
+</svelte:head>
+
+Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount / person Total / person
+Reset
