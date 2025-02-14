549a4f81bb00271f0e659ceec603003e885697af
Jaylen Baxter <14062951+JYLN@users.noreply.github.com>
2025-02-14 09:28:48 -0700

    chore: :art: add base markup and Tailwind CSS config

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import '@fontsource/space-mono/700.css';
 	import '../app.css';
 </script>
 
@@ -6,5 +7,69 @@
 	<title>Frontend Mentor | Tip Calculator</title>
 </svelte:head>
 
-Bill Select Tip % 5% 10% 15% 25% 50% Custom Number of People Tip Amount / person Total / person
-Reset
+<div>
+	<header>
+		<img src="/images/logo.svg" alt="Splitter App" />
+	</header>
+	<main>
+		<div>
+			<form>
+				<div>
+					<label for="bill">Bill</label>
+					<input id="bill" type="number" />
+				</div>
+				<div>
+					<p>Select Tip %</p>
+					<div>
+						<div>
+							<input />
+							<label>5%</label>
+						</div>
+						<div>
+							<input />
+							<label>10%</label>
+						</div>
+						<div>
+							<input />
+							<label>15%</label>
+						</div>
+						<div>
+							<input />
+							<label>25%</label>
+						</div>
+						<div>
+							<input />
+							<label>50%</label>
+						</div>
+						<div>
+							<input />
+						</div>
+					</div>
+				</div>
+				<div>
+					<label for="people">Number of People</label>
+					<input id="people" type="number" />
+				</div>
+			</form>
+			<div>
+				<div>
+					<div>
+						<div>
+							<p>Tip Amount</p>
+							<p>/ person</p>
+						</div>
+						<output>$0.00</output>
+					</div>
+					<div>
+						<div>
+							<p>Total</p>
+							<p>/ person</p>
+						</div>
+						<output>$0.00</output>
+					</div>
+				</div>
+				<button>Reset</button>
+			</div>
+		</div>
+	</main>
+</div>
